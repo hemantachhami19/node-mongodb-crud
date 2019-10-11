5299112627832f95d09bdf095bb8079dec7fbeb2
Hemant Achhami <hemantachhami19@gmail.com>
2019-10-11 18:38:24 +0545

    inserting documents

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,14 @@
 const mongodb =  require('mongodb');
 const MongoClient = mongodb.MongoClient;
 const connectionUrl = 'mongodb://127.0.0.1:27017';
-const database = 'task-manager';
+const databaseName = 'task-manager';
 MongoClient.connect(connectionUrl, {useNewUrlParser:true},(error,client)=>{
     if(error){
         return console.log("Unable to connect to database",error);
     }
-   console.log('connected successfully')
+    const db = client.db(databaseName);
+    db.collection('users').insertOne({
+        name:'Hemant',
+        age:'26'
+    });
 });
